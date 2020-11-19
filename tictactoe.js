8606ec1686d351a44fe4d3acb7b35f5fb3bcb612
Tim Kalkhof <t.kalkhof@sap.com>
2020-11-19 17:12:07 +0100

    Added draw detection and refactoring

--- tictactoe.js
+++ tictactoe.js
@@ -11,13 +11,6 @@ function setup() {
   color(255);
   strokeWeight(10);
 
-  //Create playingfield
-  for (let i = 1; i < 3; i++) {
-    let spacing = i * width / 3;
-    line(spacing, gap, spacing, width - gap);
-    line(gap, spacing, width - gap, spacing);
-  }
-
   //Calculate boundaries
   //i = col; j = row
   for (let i = 1; i < 4; i++) {
@@ -26,51 +19,50 @@ function setup() {
     }
   }
 
-  //Fill pieces to avoid early game over
-  for (let i = 0; i < 9; i++) {
-    pieces[i] = char(i);
-  }
+  initPlayfield();
+  initPieces();
 }
 
 //onMouseClick
 function mouseClicked() {
-  if (game) {
+  let pos = calcPosfromMouse(mouseX, mouseY);
+  if (game && !occupied(pos)) {
     if (prevTurn == "O") {
-      drawX(calcPosfromMouse(mouseX, mouseY));
+      drawX(pos);
+      prevTurn = "X";
+      pieces[pos] = "X";
     }
     else {
-      drawO(calcPosfromMouse(mouseX, mouseY));
+      drawO(pos);
+      prevTurn = "O";
+      pieces[pos] = "O";
     }
     if (three()) {
-      gameOver();
       game = false;
+      drawString("Game Over!\n" + prevTurn + " Wins!");
+    }
+    if(isDraw()){
+      game = false;
+      drawString("Its a draw!");
     }
   }
-  else{
-  reset();
+  else {
+    reset();
   }
 }
 
 //Draw X
 function drawX(pos) {
-  if (!occupied(pos)) {
-    prevTurn = "X";
-    pieces[pos] = "X";
-    stroke("#B09492");
-    line(positions[pos].x - width / 3 + gap, positions[pos].y - height / 3 + gap, positions[pos].x - gap, positions[pos].y - gap);
-    line(positions[pos].x - gap, positions[pos].y - height/ 3 + gap, positions[pos].x - width / 3 + gap, positions[pos].y - gap);
-  }
+  stroke("#B09492");
+  line(positions[pos].x - width / 3 + gap, positions[pos].y - height / 3 + gap, positions[pos].x - gap, positions[pos].y - gap);
+  line(positions[pos].x - gap, positions[pos].y - height / 3 + gap, positions[pos].x - width / 3 + gap, positions[pos].y - gap);
 }
 
 //Draw O
 function drawO(pos) {
-  if (!occupied(pos)) {
-    prevTurn = "O";
-    fill("#91A3B0");
-    stroke("#B0A892");
-    pieces[pos] = "O";
-    circle(positions[pos].x - width/ 6, positions[pos].y - height / 6, 100);
-  }
+  fill("#91A3B0");
+  stroke("#B0A892");
+  circle(positions[pos].x - width / 6, positions[pos].y - height / 6, 100);
 }
 
 //Calculate if there are three in a row
@@ -82,7 +74,6 @@ function three() {
 }
 
 //Check for rows
-//Refactor
 function rows() {
   if (pieces[0] == pieces[1] && pieces[1] == pieces[2]) {
     return true;
@@ -97,7 +88,6 @@ function rows() {
 }
 
 //Check for colums and draw lines
-//Refactor
 function cols() {
   if (pieces[0] == pieces[3] && pieces[3] == pieces[6]) {
     return true;
@@ -112,7 +102,6 @@ function cols() {
 }
 
 //Check for diags
-//Refactor
 function diag() {
   if (pieces[0] == pieces[4] && pieces[4] == pieces[8]) {
     return true;
@@ -127,18 +116,12 @@ function reset() {
   game = true;
   background("#91A3B0");
   stroke(0);
-  for (let i = 1; i < 3; i++) {
-    let spacing = i * width / 3;
-    line(spacing, gap, spacing, width - gap);
-    line(gap, spacing, width - gap, spacing);
-  }
-    //Fill pieces to avoid early game over
-    for (let i = 0; i < 9; i++) {
-      pieces[i] = char(i);
-    }
+  initPlayfield();
+  initPieces();
   game = true;
 }
 
+//Calculate field from mouse click position
 function calcPosfromMouse(x, y) {
   console.log("X: " + x + " Y: " + y);
   for (let pos = 0; pos < 9; pos++) {
@@ -153,12 +136,34 @@ function occupied(pos) {
   return pieces[pos] == "X" || pieces[pos] == "O";
 }
 
-function gameOver() {
+//Draw the game over text on the screen
+function drawString(msg) {
   fill(0);
   textSize(32);
   stroke(255);
   textAlign("center");
-  text("Game Over!\n" + prevTurn + " Wins!", 200, 200);
+  text(msg, width / 2, width / 2);
+}
+
+//Check if state is a draw
+function isDraw() {
+  return pieces.every(elem => ["X", "O"].includes(elem))
+}
+
+function initPlayfield() {
+  //Create playingfield
+  for (let i = 1; i < 3; i++) {
+    let spacing = i * width / 3;
+    line(spacing, gap, spacing, width - gap);
+    line(gap, spacing, width - gap, spacing);
+  }
+}
+
+function initPieces() {
+  //Fill pieces to avoid early game over
+  for (let i = 0; i < 9; i++) {
+    pieces[i] = char(i);
+  }
 }
 
 class Bounds {
